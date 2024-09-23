5878836784f041c03b8977fda4e4defc8ac43e4a
abdu-shakur <71818254+abdu-shakur@users.noreply.github.com>
2024-09-23 13:41:53 +0100

    MXRidee

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8sy9ZVYKb6pXZ7BxdeZTZZd/WHjaWI0b/nuAqxB3xt8=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NZB5sM6jsbK6Fk8G5nlTPQvtgU6BlIUrOl1BStCS87U=\"\n}"
